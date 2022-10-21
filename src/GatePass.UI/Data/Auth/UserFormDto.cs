8153489ab3a031ee755aaebf822c4c26634ac209
dreamcoder <ojambo04@gmail.com>
2022-10-21 13:42:01 +0530

    User management added

--- src/GatePass.UI/Data/Auth/UserFormDto.cs
+++ src/GatePass.UI/Data/Auth/UserFormDto.cs
@@ -3,21 +3,17 @@
 namespace GatePass.UI.Data;
 
 public class UserFormDto
-{
-    public string? Id { get; set; }
-    
+{    
     [Required] public string? UserName { get; set; }
     
     [Required] public string? DisplayName { get; set; }
 
     [Required, EmailAddress]
     public string? Email { get; set; }
-    
-    [Required] public string? Password { get; set; }
-    
-    [Required] public string? ConfirmPassword { get; set; }
-    
-    public string[]? AssignRoles { get; set; }
+
+    public Guid LocationId { get; set; }
+
+    public List<string> Roles { get; set; } = new();
     
     public bool IsActive { get; set; } = true;
 }
